4424c396af89780d29d5669208121eccad711555
alexaxms <alexaxmss@hotmail.com>
2020-02-13 19:43:23 -0300

    WIP

--- src/lane/LaneContainer.tsx
+++ src/lane/LaneContainer.tsx
@@ -1,21 +1,16 @@
 import React from "react";
-import IAppState from "../lane/store/IAppState.interface";
-import {connect} from "react-redux";
-import ILane from "./data/ILane.interface";
+import { useDispatch, useSelector} from "react-redux";
 import Loader from "../ticket/Loader";
 import {getLanesStartActionCreator} from "./actions/LaneActionCreators";
 import LaneList from "./LaneList";
 
-interface IProps {
-    getLanes: Function,
-    lanes: ILane[],
-    isFetching: Boolean
-}
+const LaneContainer = () => {
+    const dispatch = useDispatch();
+    const  {lanes , isFetching }  = useSelector((state: any) => state.laneState);
 
-const LaneContainer: React.FunctionComponent<IProps> = ({getLanes, lanes, isFetching}) => {
     React.useEffect(() => {
-        getLanes();
-    }, [getLanes]);
+        dispatch(getLanesStartActionCreator());
+    }, [dispatch]);
 
     return (
         <>
@@ -24,20 +19,6 @@ const LaneContainer: React.FunctionComponent<IProps> = ({getLanes, lanes, isFetc
     );
 };
 
-// Make data available on props
-const mapStateToProps = (store: IAppState) => {
-    return {
-        lanes: store.laneState.lanes,
-        isFetching: store.laneState.isFetching,
-    };
-};
-
-// Make functions available on props
-const mapDispatchToProps = (dispatch: any) => {
-    return {
-        getLanes: () => dispatch(getLanesStartActionCreator()),
-    }
-};
 
 // Connect the app aware container to the store and reducers
-export default connect(mapStateToProps, mapDispatchToProps)(LaneContainer);
+export default LaneContainer;
--- src/root/Root.tsx
+++ src/root/Root.tsx
@@ -4,15 +4,18 @@ import {Store} from 'redux';
 
 import App from '../app/App';
 import IAppState from '../store/IAppState.interface';
+import {Profiler} from "react";
 
 interface IProps {
     store: Store<IAppState>;
 }
 
 const Root: React.SFC<IProps> = ({store}) => (
-    <Provider store={store}>
-        <App/>
-    </Provider>
+     <Profiler id={"id"} onRender={()=> console.log('Carlitos')}>
+         <Provider store={store}>
+             <App/>
+         </Provider>
+     </Profiler>
 );
 
 export default Root;
